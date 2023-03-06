f2ca942f4f3f18129a0817c3418b87b4ef605d54
sárvári merse <merse.sarvari@gmail.com>
2023-03-06 21:16:35 +0100

    saving

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -7,20 +7,28 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
-if (servers) {
-    servers.map((server) => (
-        import server.id from ''
-    ));
-    
-}
 
 
 
 export default function Home() {
-    const [url, setURL] = useState('E:\\Programing\\MinecraftServerList\\Resources\\Files\\ServerThumbnails\\');
+    const ping_image = '..\\Resources\\Files\\Images\\ping.png';
+    const [url, setURL] = useState('..\\Resources\\Files\\ServerThumbnails\\');
     const [servers, setServers] = useState(null);
+    const videoEl = useRef(null);
 
+    const attemptPlay = () => {
+        videoEl &&
+            videoEl.current &&
+            videoEl.current.play().catch(error => {
+                console.error("Error attempting to play", error);
+            });
+    };
+
+    useEffect(() => {
+        attemptPlay();
+    }, []);
 
     async function fetchData() {
         try {
@@ -46,39 +54,55 @@ export default function Home() {
 
     return (
         <div className="home">
-            <h2>Welcome to server information:</h2>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Server Address</TableCell>
-                            <TableCell align="right">Server status</TableCell>
-                            <TableCell align="right">Version</TableCell>
-                            <TableCell align="right">Players</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {servers && servers.map((server) => (
-                            <TableRow
-                                key={server.id}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="server">
-                                    {server.ip}
-                                </TableCell>
-                                
-                                <TableCell align="right">{getserverStatus(server.status)}
+            <Grid container spacing={0} style={{ backgroundColor: '#3d4491' }} width="120">
+                <Grid item xs={12} md={12}>
+                    <h2>Welcome to server information:</h2>
+                </Grid>
+            </Grid>
+            {servers && servers.map((server) => (
+                <Grid container spacing={0} height="120px" key={server.id}
+                    style={{
+                        backgroundColor: '#292826',
+                        marginBottom: '20px',
+                        color: 'black'
+                    }}>
+                    <Grid item xs={1} md={1}>
 
-                                </TableCell>
-                                <TableCell align="right">{server.serverVersion}
-                                </TableCell>
-                                <TableCell align="right">{server.currentPlayers}/{server.maxPlayer}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                    </Grid>
+                    <Grid item xs={5} md={5} style={{ backgroundColor: 'white', color: 'black', alignItems: 'center', align: 'center' }}>
+                        <video
+                            style={{ height: "60px", width: "468px", margin: "0", padding: '25px 20px' }}
+                            autoPlay
+                            playsInline
+                            loop
+                            muted
+                            alt="All the devices"
+                            src={url + server.thumbnailPath}
+                            ref={videoEl}
+                        />
+                    </Grid>
+                    <Grid item xs={4} md={4} style={{ backgroundColor: 'white', color: 'black', alignItems: 'center', align: 'center' }} >
+                        <Grid item xs={12} md={12} height="40px" style={{ backgroundColor: 'white', color: 'black', alignItems: 'center', align: 'center' }} >
+                        </Grid>
+                        <Grid item xs={12} md={12} height="40px" style={{ backgroundColor: 'white', color: 'black', alignItems: 'center', align: 'center' }} >
+                            <img src={ping_image} width="30px" style={{ marginTop: 'auto', marginBottom: 'auto' }} />
+                            {server.currentPlayers}/{server.maxPlayer} playing now
+                        </Grid>
+                        <Grid item xs={12} md={12} height="40px"  style={{ backgroundColor: 'white', color: 'black', alignItems: 'center', align: 'center' }} >
+                        </Grid>
+                        
+                    </Grid>
+                    <Grid item xs={2} md={2} style={{ backgroundColor: '#292826' }}>
+                        <Grid item xs={12} md={12} style={{ backgroundColor: '#292826', height: '50%' }}>
+                            {server.serverVersion}
+                        </Grid>
+                        <Grid item xs={12} md={12} style={{ backgroundColor: '#128f0b', height: '50%' }}>
+                            {server.ip}
+                        </Grid>
 
+                    </Grid>
+                </Grid>
+            ))}
         </div>
     )
 }
