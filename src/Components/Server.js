374262e86acaf108eea9fdf96a8d08b1c8bc9613
sárvári merse <merse.sarvari@gmail.com>
2023-03-09 23:09:52 +0100

    saving new text formats

--- src/Components/Server.js
+++ src/Components/Server.js
@@ -2,6 +2,12 @@ import Grid from '@mui/material/Grid';
 import Avatar from '@mui/material/Avatar';
 import axios from "axios";
 import { useEffect, useState, useRef } from "react";
+import BoyIcon from '@mui/icons-material/Boy';
+import Man2Icon from '@mui/icons-material/Man2';
+
+//style
+import '../style/css/googlefonts.css';
+import { fontStyle } from '@mui/system';
 
 const Server = (props) => {
     const ping_image = '..\\Resources\\Files\\Images\\ping.png';
@@ -36,25 +42,16 @@ const Server = (props) => {
                 </Grid>
                 <Grid item xs={4} md={4} style={{ backgroundColor: 'white', color: 'black' }} >
                     <div style={{ backgroundColor: 'white', color: 'black', whiteSpace: 'nowrap', marginTop:'20px'}}>
-                        <Grid item xs={12} md={12} height="20px" style={{color:'black', fontSize:"20px"}}>
+                        <Grid item xs={12} md={12} height="20px" style={{color:'black', fontSize:"20px", marginBottom:'8px', fontWeight: '800',}}>
                             { server.servername}
                         </Grid>
-                        <Grid item xs={12} md={12} height="20px" style={{color:'black'}}>
-                        <div style={{
-                            display: 'inline-flex',
-                            VerticalAlign: 'text-bottom',
-                            BoxSizing: 'inherit',
-                            textAlign: 'center',
-                            AlignItems: 'center',
-                            color:'black'
-                        }}>
-                            <img alt="Remy Sharp" src={ping_image} style={{width:'28px'}}/>
-                            {server.currentPlayers}/{server.maxPlayer} playing now
-                        </div> 
+                        <Grid item xs={12} md={12} height="15px" style={{color:'black', fontSize:"14px"}}>
+                        {server.currentPlayers}/{server.maxPlayer} playing now
+                            
                                 
                                 
                         </Grid>
-                        <Grid item xs={12} md={12} height="20px" style={{color:'black'}}>
+                        <Grid item xs={12} md={12} height="15px" style={{color:'black', fontSize:"14px"}}>
                             { server.serverVersion}
                         </Grid>
                     </div>
@@ -62,7 +59,7 @@ const Server = (props) => {
                     
                 </Grid>
                 <Grid item xs={2} md={2} style={{ backgroundColor: '#292826', textAlign: 'center'}}>
-                    <Grid item xs={12} md={12} style={{ backgroundColor: '#292826', height: '50%', textAlign: 'center' }}>
+                    <Grid item xs={12} md={12} style={{ backgroundColor: '#292826', height: '50%', textAlign: 'center', fontSize:"14px", paddingTop:'20px'}}>
                         {server.ip}
                     </Grid>
                     <Grid item xs={12} md={12} style={
@@ -70,6 +67,9 @@ const Server = (props) => {
                             backgroundColor: '#128f0b',
                             height: '50%',
                             textAlign: 'center',
+                            fontSize:"18px", 
+                            paddingTop:'18px',
+                            fontStyle: 'bold',
                         }}>
 
                         COPY
