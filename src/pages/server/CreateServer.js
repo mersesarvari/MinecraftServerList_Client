0d6cb34b2599346a30bd08f4f3a34e9c57cd3cee
sárvári merse <merse.sarvari@gmail.com>
2023-03-24 09:41:00 +0100

    adding the steps

--- src/pages/server/CreateServer.js
+++ src/pages/server/CreateServer.js
@@ -27,16 +27,11 @@ import NavForm from "../../Components/navigationform";
 import CountryAutoSelect from "../../Components/CountryAutoSelect";
 import { ServerFormScheme } from "../../validations/ValidationSchemes";
 
-const theme = createTheme();
-
 export default function CreateServer() {
   const steps = ["Information", "Description", "Social"];
-  const FormTitle = ["Details", "Description", "Social"];
-  const [page, setPage] = useState(0);
   const serverTypes = ["java", "bedrock"];
   const [javacheck, setJavacheck] = useState(false);
   const [bedrockcheck, setBedrockcheck] = useState(false);
-
   const [activeStep, setActiveStep] = React.useState(0);
   const [completed, setCompleted] = React.useState({});
 
@@ -44,26 +39,16 @@ export default function CreateServer() {
     return steps.length;
   };
 
-  const completedSteps = () => {
-    return Object.keys(completed).length;
-  };
-
   const isLastStep = () => {
     return activeStep === totalSteps() - 1;
   };
 
-  const allStepsCompleted = () => {
-    return completedSteps() === totalSteps();
-  };
-
   const handleNext = () => {
-    const newActiveStep =
-      isLastStep() && !allStepsCompleted()
-        ? // It's the last step, but not all steps have been completed,
-          // find the first step that has been completed
-          steps.findIndex((step, i) => !(i in completed))
-        : activeStep + 1;
-    setActiveStep(newActiveStep);
+    if (isLastStep()) {
+      alert("You reached the last stem on this form");
+      return;
+    }
+    setActiveStep((nextActiveStep) => nextActiveStep + 1);
   };
 
   const handleBack = () => {
@@ -72,13 +57,6 @@ export default function CreateServer() {
 
   const handleStep = (step) => () => {
     setActiveStep(step);
-  };
-
-  const handleComplete = () => {
-    const newCompleted = completed;
-    newCompleted[activeStep] = true;
-    setCompleted(newCompleted);
-    handleNext();
   };
 
   const handleReset = () => {
@@ -285,150 +263,74 @@ export default function CreateServer() {
   };
 
   return (
-    /*
-        <>
-          <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-              <NavForm />
-              <CssBaseline />
-              <Formik
-                initialValues={{
-                  servername: "",
-                  serverjavaip: "",
-                  serverjavaport: "25565",
-                  serverbedrockip: "",
-                  serverbedrockport: "",
-                }}
-                onSubmit={(values, formik) => {
-                  if (
-                    values.serverjavaip === "" &&
-                    values.serverbedrockip === ""
-                  ) {
-                    return;
-                  }
+    <Container component="main" maxWidth="sm">
+      <NavForm />
+      <CssBaseline />
+      <Box sx={{ width: "100%" }}>
+        <Stepper nonLinear activeStep={activeStep}>
+          {steps.map((label, index) => (
+            <Step key={label} completed={completed[index]}>
+              <StepButton color="inherit" onClick={handleStep(index)}>
+                {label}
+              </StepButton>
+            </Step>
+          ))}
+        </Stepper>
+        <React.Fragment>
+          <Formik
+            initialValues={{
+              servername: "",
+              serverjavaip: "",
+              serverjavaport: "25565",
+              serverbedrockip: "",
+              serverbedrockport: "",
+            }}
+            onSubmit={(values, formik) => {
+              if (values.serverjavaip === "" && values.serverbedrockip === "") {
+                return;
+              }
 
-                  console.log(JSON.stringify(values));
-                  alert("Next page");
-                  Next();
-                }}
-                validationSchema={ServerFormScheme}
-              >
-                {(formik) => (
-                  <form onSubmit={formik.handleSubmit}>
-                    {page === 0 && (
-                      <Details list={serverTypes} formik={formik} />
-                    )}
-                    {page === 1 && (
-                      <Description list={serverTypes} formik={formik} />
-                    )}
-                    {page === 2 && (
-                      <Social list={serverTypes} formik={formik} />
-                    )}
-                  </form>
+              console.log(JSON.stringify(values));
+              alert("Next page");
+            }}
+            validationSchema={ServerFormScheme}
+          >
+            {(formik) => (
+              <form onSubmit={formik.handleSubmit}>
+                {activeStep === 0 && (
+                  <Details list={serverTypes} formik={formik} />
                 )}
-              </Formik>
-            </Container>
-          </ThemeProvider>
-        </>
-        */
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="sm">
-        <NavForm />
-        <CssBaseline />
-        <Box sx={{ width: "100%" }}>
-          <Stepper nonLinear activeStep={activeStep}>
-            {steps.map((label, index) => (
-              <Step key={label} completed={completed[index]}>
-                <StepButton color="inherit" onClick={handleStep(index)}>
-                  {label}
-                </StepButton>
-              </Step>
-            ))}
-          </Stepper>
-          <div>
-            {allStepsCompleted() ? (
-              <React.Fragment>
-                <Typography sx={{ mt: 2, mb: 1 }}>
-                  All steps completed - you&apos;re finished
-                </Typography>
-                <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-                  <Box sx={{ flex: "1 1 auto" }} />
-                  <Button onClick={handleReset}>Reset</Button>
-                </Box>
-              </React.Fragment>
-            ) : (
-              <React.Fragment>
-                <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
-                  <Formik
-                    initialValues={{
-                      servername: "",
-                      serverjavaip: "",
-                      serverjavaport: "25565",
-                      serverbedrockip: "",
-                      serverbedrockport: "",
-                    }}
-                    onSubmit={(values, formik) => {
-                      if (
-                        values.serverjavaip === "" &&
-                        values.serverbedrockip === ""
-                      ) {
-                        return;
-                      }
-
-                      console.log(JSON.stringify(values));
-                      alert("Next page");
-                    }}
-                    validationSchema={ServerFormScheme}
-                  >
-                    {(formik) => (
-                      <form onSubmit={formik.handleSubmit}>
-                        {activeStep === 0 && (
-                          <Details list={serverTypes} formik={formik} />
-                        )}
-                        {activeStep === 1 && (
-                          <Description list={serverTypes} formik={formik} />
-                        )}
-                        {activeStep === 2 && (
-                          <Social list={serverTypes} formik={formik} />
-                        )}
-                      </form>
-                    )}
-                  </Formik>
-                </Typography>
-                <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-                  <Button
-                    color="inherit"
-                    disabled={activeStep === 0}
-                    onClick={handleBack}
-                    sx={{ mr: 1 }}
-                  >
-                    Back
-                  </Button>
-                  <Box sx={{ flex: "1 1 auto" }} />
-                  <Button onClick={handleNext} sx={{ mr: 1 }}>
-                    Next
-                  </Button>
-                  {activeStep !== steps.length &&
-                    (completed[activeStep] ? (
-                      <Typography
-                        variant="caption"
-                        sx={{ display: "inline-block" }}
-                      >
-                        Step {activeStep + 1} already completed
-                      </Typography>
-                    ) : (
-                      <Button onClick={handleComplete}>
-                        {completedSteps() === totalSteps() - 1
-                          ? "Finish"
-                          : "Complete Step"}
-                      </Button>
-                    ))}
-                </Box>
-              </React.Fragment>
+                {activeStep === 1 && (
+                  <Description list={serverTypes} formik={formik} />
+                )}
+                {activeStep === 2 && (
+                  <Social list={serverTypes} formik={formik} />
+                )}
+              </form>
             )}
-          </div>
-        </Box>
-      </Container>
-    </ThemeProvider>
+          </Formik>
+          <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+            <Button
+              color="inherit"
+              disabled={activeStep === 0}
+              onClick={handleBack}
+              sx={{ mr: 1 }}
+            >
+              Back
+            </Button>
+            <Box sx={{ flex: "1 1 auto" }} />
+            {isLastStep ? (
+              <Button onClick={handleNext} sx={{ mr: 1 }}>
+                Next
+              </Button>
+            ) : (
+              <Button onClick={handleNext} sx={{ mr: 1 }}>
+                Complete
+              </Button>
+            )}
+          </Box>
+        </React.Fragment>
+      </Box>
+    </Container>
   );
 }
